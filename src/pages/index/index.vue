c51362f39b823625ea7c6c8977a76bd04225a98d
张晓刚 <zhangxiaogang@idea-group.cn>
2025-01-06 10:14:26 +0800

    前台分类数据定义与渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,26 +1,40 @@
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
-import { getHomeBannerAPI } from '@/services/home'
-import type { BannerItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 
 const bannerList = ref<BannerItem[]>([])
+//获得轮播图
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
-
   bannerList.value = res.result
+}
+//获取前台分类
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
 }
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel :list="categoryList" />
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
